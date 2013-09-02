ac1bbf72c5f9218eb18f666dfe8355b826980429
Csaba Okrona <ochronus@ochronus.com>
2013-09-02 12:13:26 +0200

    static is static again

--- tweedli_web/settings/dev.py
+++ tweedli_web/settings/dev.py
@@ -78,7 +78,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = normpath(join(SITE_ROOT, 'assets'))
+STATIC_ROOT = normpath(join(SITE_ROOT, 'static'))
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
