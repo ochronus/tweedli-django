fd7201af86119f64cd293dbd02eb1a9c0ef0f1e2
Csaba Okrona <ochronus@ochronus.com>
2013-09-03 10:18:16 +0200

    host connection

--- tweedli_web/settings/prod.py
+++ tweedli_web/settings/prod.py
@@ -18,7 +18,7 @@ DATABASES = {
         'PASSWORD': 'tweedli',
         # Empty for localhost through domain sockets or '127.0.0.1' for
         # localhost through TCP.
-        'HOST': '',
+        'HOST': '127.0.0.1',
         'PORT': '',                      # Set to empty string for default.
     }
 }
