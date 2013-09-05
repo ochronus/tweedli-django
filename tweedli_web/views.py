3c78d4c4402d9150343a010b187e528aad3c0709
Csaba Okrona <ochronus@ochronus.com>
2013-09-05 06:03:59 +0200

    fixing formatting

--- tweedli_web/views.py
+++ tweedli_web/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 def index(request):
     if request.user.is_authenticated():
         return render(request, 'main.html', {"foo": "bar"},
-                    content_type="text/html")
+                      content_type="text/html")
     else:
         return render(request, 'index.html', {"foo": "bar"},
-                    content_type="text/html")
+                      content_type="text/html")
