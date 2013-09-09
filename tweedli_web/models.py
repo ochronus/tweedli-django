5ab973e9596cf0cfa3b26b6279c68766dbf1e23a
Csaba Okrona <ochronus@ochronus.com>
2013-09-09 14:30:33 +0200

    pep8 fixes

--- tweedli_web/models.py
+++ tweedli_web/models.py
@@ -3,15 +3,18 @@ from django.contrib.auth.models import User
 
 
 class Schedule(models.Model):
-    when = models.PositiveIntegerField(db_index=True)                    #GMT
-    tz_compensation = models.BigIntegerField(default=0, db_index=True)     # +/- seconds from GMT time
+    when = models.PositiveIntegerField(db_index=True)  # GMT
+    # +/- seconds from GMT time
+    tz_compensation = models.BigIntegerField(default=0, db_index=True)
     user = models.ForeignKey(User, db_index=True)
+
 
 class Tweet(models.Model):
     text = models.CharField(max_length=140)
     user = models.ForeignKey(User, db_index=True)
     order_id = models.SmallIntegerField(db_index=True)
     tweeted_at = models.PositiveIntegerField(default=None)
+
 
 class TweetImage(models.Model):
     url = models.CharField(max_length=4096)
